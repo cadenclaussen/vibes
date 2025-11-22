bd58d224a3de375a62dfca3050772030442589b2
Caden Claussen <cadenclaussen@users.noreply.github.com>
2025-11-22 13:39:10 -0800

    Initial Commit

---
+++ vibes/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  vibes
+//
+//  Created by Caden Claussen on 11/22/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
